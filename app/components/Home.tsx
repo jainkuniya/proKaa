b2fbee7a3035ba467a248f8a75c357427ec21328
jainkuniya <jainkuniya@gmail.com>
2020-03-28 17:39:08 +0530

    wip-setup up simple producer

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,13 +1,98 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes.json';
+import React, { PureComponent } from 'react';
+import AceEditor from 'react-ace';
+import kafka from 'kafka-node';
+
 import styles from './Home.css';
 
-export default function Home() {
-  return (
-    <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
-    </div>
-  );
+type State = {
+  message: string;
+  host: string;
+};
+
+type Props = {};
+
+export default class Home extends PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      message: '',
+      host: ''
+    };
+  }
+
+  onChange = (message: string) => {
+    this.setState({
+      message
+    });
+  };
+
+  handleHostChange = (host: { target: { value: string } }) => {
+    this.setState({
+      host: host.target.value
+    });
+  };
+
+  sendMessage = () => {
+    console.log('Fdasfsad');
+    const { KeyedMessage, Producer } = kafka;
+    const client = new kafka.KafkaClient();
+    const producer = new Producer(client);
+    const km = new KeyedMessage('key', 'message');
+    const payloads = [
+      { topic: 'topic1', messages: 'hi', partition: 0 },
+      { topic: 'topic2', messages: ['hello', 'world', km] }
+    ];
+    producer.on('ready', () => {
+      producer.send(payloads, (err, data) => {
+        console.log(data);
+      });
+    });
+
+    producer.on('error', err => {
+      console.log(err);
+    });
+  };
+
+  onLoad = () => {};
+
+  render() {
+    const { host, message } = this.state;
+    return (
+      <div className={styles.container} data-tid="container">
+        <input
+          value={host}
+          className={styles.addressBar}
+          placeholder="kafkaHost:9092"
+          onChange={e => {
+            this.handleHostChange(e);
+          }}
+        />
+        <hr />
+        <AceEditor
+          placeholder="Placeholder Text"
+          mode="javascript"
+          theme="monokai"
+          name="blah2"
+          onLoad={this.onLoad}
+          onChange={this.onChange}
+          fontSize={14}
+          showPrintMargin
+          showGutter
+          highlightActiveLine
+          value={message}
+          setOptions={{
+            enableBasicAutocompletion: false,
+            enableLiveAutocompletion: false,
+            enableSnippets: false,
+            showLineNumbers: true,
+            tabSize: 2
+          }}
+        />
+
+        <button type="button" onClick={this.sendMessage}>
+          Push
+        </button>
+      </div>
+    );
+  }
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,8 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
-import Root from './containers/Root';
-import { configureStore, history } from './store/configureStore';
 import './app.global.css';
-
-const store = configureStore();
-
-const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
+import Home from './components/Home';
 
 document.addEventListener('DOMContentLoaded', () =>
-  render(
-    <AppContainer>
-      <Root store={store} history={history} />
-    </AppContainer>,
-    document.getElementById('root')
-  )
+  render(<Home />, document.getElementById('root'))
 );
